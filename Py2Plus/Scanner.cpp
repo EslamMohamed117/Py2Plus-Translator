15e1573647a4a2392f14ffd3f29ea903bfd4f03b
xS4yk0x <sayko117@gmail.com>
2023-05-09 21:13:49 +0300

    Scanner API to tokenize input source code

---
+++ Py2Plus/Scanner.cpp
@@ -0,0 +1,76 @@
+#include "Scanner.h"
+
+#include <iostream>
+#include <string>
+using namespace std;
+using namespace boost;
+
+
+string code;
+bool Scanner::isoperator(char c) {
+    static const std::string operators = "+-*/%=><!&|^~";
+    return operators.find(c) != std::string::npos;
+}
+
+bool Scanner::tokenize()
+{
+    try {
+        char_separator<char> sep; // default constructed
+        boostTokenizer tokenList(this->code, sep);
+        for (boostTokenizer::iterator token_iter = tokenList.begin(); token_iter != tokenList.end(); ++token_iter)
+            this->tokenList.push_back(classifyToken(*token_iter));
+        return true;
+    }
+    catch (const std::exception& e) {
+        // Handle the exception here.
+        std::cerr << "Tokenizer error: " << e.what() << std::endl;
+        return false;
+    }
+}
+
+TokenInfo Scanner::classifyToken(const std::string& tokenStr) {
+    if (tokenStr.empty()) {
+        return { tokens[TOKEN_END_OF_FILE], "" };
+    }
+
+    auto it = keywords.find(tokenStr);
+    if (it != keywords.end()) return { keywords.at(tokenStr), tokenStr };
+
+    char c = tokenStr[0];
+    if (std::isspace(c)) {
+        return { tokens[TOKEN_WHITESPACE], tokenStr };
+    }
+
+    if (!std::isalnum(c) && !isoperator(c) && c != '_') {
+        return { tokens[TOKEN_INVALID], tokenStr };
+    }
+
+    if (std::isdigit(c)) {
+        std::string value(tokenStr);
+        while (value.size() < tokenStr.size() && std::isdigit(tokenStr[value.size()])) {
+            value += tokenStr[value.size()];
+        }
+        return { tokens[TOKEN_NUMBER], value };
+    }
+    if (c == '"' || c == '\'' || (std::isalnum(c) && !std::isdigit(c))) {
+        std::string value(tokenStr);
+        while (value.size() < tokenStr.size() && value.back() != c) {
+            value += tokenStr[value.size()];
+        }
+        return { tokens[TOKEN_STRING], value };
+    }
+
+    if (isoperator(c)) {
+        return { tokens[TOKEN_OPERATOR], tokenStr };
+    }
+
+    static const std::string separators = ".,:;()[]{}";
+    if (separators.find(c) != std::string::npos) {
+        std::string value(tokenStr);
+        std::string tokenStr = "TOKEN_SEPARATOR_" + value;
+        Token token = static_cast<Token>(std::hash<std::string>{}(tokenStr));
+        return { tokens[token], value };
+    }
+
+    return { tokens[TOKEN_INVALID], tokenStr };
+}
---
+++ Py2Plus/Scanner.h
@@ -0,0 +1,97 @@
+#pragma once
+#include <boost/tokenizer.hpp>
+#include <map>
+
+typedef boost::tokenizer<boost::char_separator<char> > boostTokenizer;
+
+const std::string tokens[] = {
+        "TOKEN_END_OF_FILE",
+        "TOKEN_WHITESPACE",
+        "TOKEN_INVALID",
+        "TOKEN_NUMBER",
+        "TOKEN_IDENTIFIER",
+        "TOKEN_STRING",
+        "TOKEN_OPERATOR",
+        "TOKEN_SEPARATOR",
+        "KEYWORD_IMPORT",
+        "KEYWORD_AND",
+        "KEYWORD_OR",
+        "KEYWORD_NONE",
+        "KEYWORD_CONTINUE",
+        "KEYWORD_BREAK",
+        "KEYWORD_FOR",
+        "KEYWORD_IN",
+        "KEYWORD_WHILE",
+        "KEYWORD_DEF",
+        "KEYWORD_IF",
+        "KEYWORD_ELIF",
+        "KEYWORD_PRINT",
+        "KEYWORD_RETURN" };
+
+const std::map<std::string, std::string> keywords = {
+       {"import"    ,   "KEYWORD_IMPORT"      },
+       {"and"       ,      "KEYWORD_AND"      },
+       {"or"        ,       "KEYWORD_OR"      },
+       {"None"      ,     "KEYWORD_NONE"      },
+       {"continue"  , "KEYWORD_CONTINUE"      },
+       {"break"     ,    "KEYWORD_BREAK"      },
+       {"for"       ,      "KEYWORD_FOR"      },
+       {"in"        ,       "KEYWORD_IN"      },
+       {"while"     ,    "KEYWORD_WHILE"      },
+       {"def"       ,      "KEYWORD_DEF"      },
+       {"if"        ,       "KEYWORD_IF"      },
+       {"elif"      ,     "KEYWORD_ELIF"      },
+       {"print"     ,    "KEYWORD_PRINT"      },
+       {"return"    ,   "KEYWORD_RETURN"      }
+};
+
+enum Token {
+    TOKEN_END_OF_FILE,
+    TOKEN_WHITESPACE,
+    TOKEN_INVALID,
+    TOKEN_NUMBER,
+    TOKEN_IDENTIFIER,
+    TOKEN_STRING,
+    TOKEN_OPERATOR,
+    TOKEN_SEPARATOR,
+    KEYWORD_IMPORT,
+    KEYWORD_AND,
+    KEYWORD_OR,
+    KEYWORD_NONE,
+    KEYWORD_CONTINUE,
+    KEYWORD_BREAK,
+    KEYWORD_FOR,
+    KEYWORD_IN,
+    KEYWORD_WHILE,
+    KEYWORD_DEF,
+    KEYWORD_IF,
+    KEYWORD_ELIF,
+    KEYWORD_PRINT,
+    KEYWORD_RETURN,
+    //    KEYWORD_GLOBAL,
+    //    KEYWORD_LAMBDA,
+    //    KEYWORD_PASS,
+    //    KEYWORD_RAISE,
+    //    KEYWORD_REP,                        // Unused keywords
+    //    KEYWORD_TRY,
+    //    KEYWORD_WITH,
+    //    KEYWORD_VAR,
+    //    KEYWORD_YIELD
+};
+
+struct TokenInfo {
+    std::string token;
+    std::string tokenValue;
+};
+
+class Scanner
+{
+private:
+    std::string code;
+    bool isoperator(char c);
+public:
+    std::vector<TokenInfo> tokenList;
+    Scanner(std::string code) { this->code = code; }
+    bool tokenize();
+    TokenInfo classifyToken(const std::string& tokenStr);
+};
