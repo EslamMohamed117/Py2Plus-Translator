3871ae819ed935d6de9ac61ce277206d4dc779ef
xS4yk0x <sayko117@gmail.com>
2023-05-09 21:01:08 +0300

    Testing area

--- Py2Plus/Py2Plus.cpp
+++ Py2Plus/Py2Plus.cpp
@@ -1,9 +1,20 @@
 #include "Py2Plus.h"
+#include <iostream>
+#include "Files.h"
+#include <QMessageBox>
+#include <string>
+using namespace std;
 
 Py2Plus::Py2Plus(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+	
+	// ------------------ THIS PART IS USED FOR TESTING PURPOSES ONLY ------------------
+	Files* file = new Files("test.py", 'r');
+    file->readFromFile();
+    QMessageBox::information(this, "Button Clicked", file->getCode().c_str());
+	// ---------------------------------------------------------------------------------
 }
 
 Py2Plus::~Py2Plus()
